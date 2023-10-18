dd6ae5738b7596fcbf6a5760c62bce1528fbda4b
Enrique Avalos <keex@Enriques-Air.lan>
2023-10-17 18:55:37 -0700

    first try

---
+++ data.js
@@ -0,0 +1,47 @@
+const people = [
+    {
+      img: 'https://m.media-amazon.com/images/I/71CEW2OokEL._SY522_.jpg',
+      name: 'Light One On and Pop One Open',
+      job: 'Eric Cocoletzi',
+      text: `The opening salvo. The 1st book in the collection. `,
+      site: 'https://www.amazon.com/Light-One-Pop-Open/dp/1545217866/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1697585795&sr=8-2',
+    },
+    {
+      img: 'https://m.media-amazon.com/images/I/A1pYHIsGS1L._SY522_.jpg',
+      name: 'They Say Animals Can Take Care of Themselves',
+      job: 'Eric Cocoletzi',
+      text: `A tour de force.`,
+      site: 'https://www.amazon.com/They-Animals-Take-Care-Themselves/dp/1985398338/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1697585843&sr=8-6',
+
+    },
+    {
+      img: 'https://m.media-amazon.com/images/I/910Z4j5rqqL._SY522_.jpg',
+      name: 'French Park',
+      job: 'Eric Cocoletzi',
+      text: `A novel for the soul.`,
+      site: 'https://www.amazon.com/FRENCH-PARK-Eric-Cocoletzi/dp/B099C14RG8/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1688060672&sr=8-1',
+    },
+    {
+      img: 'https://m.media-amazon.com/images/I/81P+oDGP4HL._SY522_.jpg',
+      name: 'Might As Well',
+      job: 'Eric Cocoletzi',
+      text: 'Beer is good.',
+      site: 'https://www.amazon.com/MIGHT-AS-WELL-Eric-Cocoletzi/dp/1795732288/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1688063231&sr=8-1-fkmr0',
+    },
+    {
+      img: 'https://m.media-amazon.com/images/I/9102hObxTJL._SY522_.jpg',
+      name: "Someplace I've Never Been To, But Would Like To Someday",
+      job: 'Eric Cocoletzi',
+      text: 'The human condition.',
+      site: 'https://www.amazon.com/Someplace-Never-Been-Would-Someday-ebook/dp/B0C4ZXBJ9D/ref=sr_1_1?crid=1VCN7S3SUBCFT&keywords=eric+cocoletzi+someplace&qid=1688063186&sprefix=eric+cocoletzi+someplace%2Caps%2C177&sr=8-1',
+    },
+    {
+      img: 'https://m.media-amazon.com/images/I/71KfJVAcZfL._SY522_.jpg',
+      name: 'Souls Are Chameleonesque',
+      job: 'Eric Cocoletzi',
+      text: 'Schools of Fish.',
+      site: 'https://www.amazon.com/dp/B0CF4NYK3F?ref_=cm_sw_r_mwn_dp_7M2AMWFSQ39BCD923CAN&language=en_US',
+    }
+  ];
+  
+  export default people;
